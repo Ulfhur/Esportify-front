04a463cc79a4b91a0d5e20da02277826f7eff201
kev <kevmunoz03@gmail.com>
2025-12-18 17:55:39 +0100

    ADD all login and register pages and UPDATE CSS

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("#/", "Accueil", "pages/home.html", "js/home.js"),
   new Route("#/registerChoice", "Register Choice", "pages/registerChoice.html", ""),
-  new Route("#/login", "Login", "pages/login.html", "")
+  new Route("#/login", "Login", "pages/login.html", ""),
+  new Route("#/registerPlayer", "Register Player", "pages/registerPlayer.html", ""),
+  new Route("#/registerOrga", "Register Orga", "pages/registerOrga.html", ""),
+  new Route("#/validateRegister", "Validate Register", "pages/validateRegister.html", ""),
 ];
 
 export const websiteName = "Esportify";
